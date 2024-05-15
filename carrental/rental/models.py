d7ee48ddb3fe024d6317b24deb53fdbf8ad927b3
katwojppuz <katarzyna.wojcik@ppuz.edu.pl>
2024-05-15 13:32:48 +0200

    2024-05-08 KW

--- carrental/rental/models.py
+++ carrental/rental/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class Equipment(models.Model):
     equipment = models.CharField(max_length=50)
@@ -25,11 +28,29 @@ class Car(models.Model):
     engine_power = models.PositiveSmallIntegerField()
     color = models.CharField(max_length=20)
     equipment = models.ManyToManyField(Equipment)
-    gearbox_type = models.CharField(max_length=20, choices=ENGINE_TYPES)
+    gearbox_type = models.CharField(max_length=20, choices=GEARBOX_TYPES)
     available = models.BooleanField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
 
-class User(models.Model):
-    pass
+class Address(models.Model):
+    country = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    post_code = models.CharField(max_length=10)
+    street = models.CharField(max_length=50)
+    building_no = models.CharField(max_length=10)
+    appartment_no = models.CharField(max_length=10)
+
+class User(User):
+    IDENTITY_DOCUMENT_TYPES = [
+        ("dowod", "dowód osobisty"),
+        ("paszport", "paszport"),
+        ("prawo_jazdy", "prawo jazdy")
+    ]
+    phone = models.CharField(max_length=20)
+    identity_document_type = models.CharField(max_length=20, choices=IDENTITY_DOCUMENT_TYPES)
+    identity_document_no = models.CharField(max_length=20)
+    address = models.OneToOneField(Address, on_delete=models.RESTRICT)
 
 class Order(models.Model):
     PAYMENT_METHODS = [
@@ -48,4 +69,14 @@ class Order(models.Model):
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
     payment_status = models.BooleanField()
 
+class RegistrationForm(ModelForm):
+    class Meta:
+        model = User
+        exclude = ['id']
+        # fields = ['first_name', 'last_name', 'email', 'phone', 'address']
 
+class AddressForm(ModelForm):
+    class Meta:
+        model = Address
+        exclude = ['id']
+        # fields = ['country', 'city', 'post_code', 'street', 'building_no', 'appartment_no']
